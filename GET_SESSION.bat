a1243440878b410c62f36b6ee9a753a8186d06ff
ermatov0019-glitch <ermatov0019@gmail.com>
2026-05-02 13:30:06 +0300

    Auto upload GET_SESSION.bat

---
+++ GET_SESSION.bat
@@ -0,0 +1,10 @@
+@echo off
+title STRING_SESSION Olish
+echo.
+echo [!] DIQQAT: Hozir sizdan telefon raqami va Telegram kodi so'raladi.
+echo [!] Bu kod Render-da UserBot ishlashi uchun zarur.
+echo.
+python generate_session.py
+echo.
+echo [!] Agar hammasi muvaffaqiyatli bo'lsa, yuqoridagi kodni nusxalab Render-ga qo'ying.
+pause
